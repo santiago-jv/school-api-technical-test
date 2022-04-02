b120e8c8d1dbfe2a2d381d6c4235bc5c98b9932e
santiago-jv <santiagoolayojv@gmail.com>
2022-04-02 16:29:20 -0500

    production config

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,12 @@
 require('dotenv').config()
 module.exports = {
-  development: {
-    client: 'mysql',
-    connection: {
-      host: process.env.DATABASE_HOST,
-      database: process.env.DATABASE_NAME,
-      user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD
-    }
+
+  client: 'mysql',
+  connection: {
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD
   },
   migrations: {
     tableName: 'knex_migrations'
